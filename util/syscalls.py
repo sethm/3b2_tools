88b91ac3df89a2ddef63e0cfbb73ae837c667a79
Seth Morabito <web@loomcom.com>
2018-04-29 10:55:14 -0700

    Revert "Syscall tracker"
    This reverts commit 24404b0243eae3d90a08bd2fd026fb3094f466a8.

--- util/syscalls.py
+++ util/syscalls.py
@@ -1,139 +1,152 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import sys
-import re
+from __future__ import print_function
+import sys, re
+from collections import namedtuple
 
-calls = [
-    'nosys',
-    'rexit',
-    'fork',
-    'read',
-    'write',
-    'open',
-    'close',
-    'wait',
-    'creat',
-    'link',
-    'unlink',
-    'exec',
-    'chdir',
-    'gtime',
-    'mknod',
-    'chmod',
-    'chown',
-    'sbreak',
-    'stat',
-    'seek',
-    'getpid',
-    'smount',
-    'sumount',
-    'setuid',
-    'getuid',
-    'stime',
-    'ptrace',
-    'alarm',
-    'fstat',
-    'pause',
-    'utime',
-    'stty',
-    'gtty',
-    'saccess',
-    'nice',
-    'statfs',
-    'sync',
-    'kill',
-    'fstatfs',
-    'setpgrp',
-    'nosys',
-    'dup',
-    'pipe',
-    'times',
-    'profil',
-    'lock',
-    'setgid',
-    'getgid',
-    'ssig',
-    'msgsys',
-    'sys3b',
-    'sysacct',
-    'shmsys',
-    'semsys',
-    'ioctl',
-    'uadmin',
-    'nosys',
-    'utssys',
-    'nosys',
-    'exece',
-    'umask',
-    'chroot',
-    'fcntl',
-    'ulimit',
-    'nosys',
-    'nosys',
-    'nosys',
-    'nosys',
-    'nosys',
-    'nosys',
-    'advfs',
-    'unadvfs',
-    'rmount',
-    'rumount',
-    'rfstart',
-    'nosys',
-    'rdebug',
-    'rfstop',
-    'rfsys',
-    'rmdir',
-    'mkdir',
-    'getdents',
-    'libattach',
-    'libdetach',
-    'sysfs',
-    'getmsg',
-    'putmsg',
-    'poll'
+# Each entry has:
+#   - Number of arguments
+#   - Name of routine
+syscalls = [
+    (0, 0, 'nosys'),
+    (1, 1, 'rexit'),
+    (2, 0, 'fork'),
+    (3, 3, 'read'),
+    (4, 3, 'write'),
+    (5, 3, 'open'),
+    (6, 1, 'close'),
+    (7, 0, 'wait'),
+    (8, 2, 'creat'),
+    (9, 2, 'link'),
+    (0, 1, 'unlink'),
+    (11, 2, 'exec'),
+    (12, 1, 'chdir'),
+    (13, 0, 'gtime'),
+    (14, 3, 'mknod'),
+    (15, 2, 'chmod'),
+    (16, 3, 'chown'),
+    (17, 1, 'sbreak'),
+    (18, 2, 'stat'),
+    (19, 3, 'seek'),
+    (20, 0, 'getpid'),
+    (21, 4, 'smount'),
+    (22, 1, 'sumount'),
+    (23, 1, 'setuid'),
+    (24, 0, 'getuid'),
+    (25, 1, 'stime'),
+    (26, 4, 'ptrace'),
+    (27, 1, 'alarm'),
+    (28, 2, 'fstat'),
+    (29, 0, 'pause'),
+    (30, 2, 'utime'),
+    (31, 2, 'stty'),
+    (32, 2, 'gtty'),
+    (33, 2, 'saccess'),
+    (34, 1, 'nice'),
+    (35, 4, 'statfs'),
+    (36, 0, 'sync'),
+    (37, 2, 'kill'),
+    (38, 4, 'fstatfs'),
+    (39, 1, 'setpgrp'),
+    (40, 0, 'nosys'),
+    (41, 1, 'dup'),
+    (42, 0, 'pipe'),
+    (43, 1, 'times'),
+    (44, 4, 'profil'),
+    (45, 1, 'lock'),
+    (46, 1, 'setgid'),
+    (47, 0, 'getgid'),
+    (48, 2, 'ssig'),
+    (49, 6, 'msgsys'),
+    (50, 4, 'sys3b'),
+    (51, 1, 'sysacct'),
+    (52, 4, 'shmsys'),
+    (53, 5, 'semsys'),
+    (54, 3, 'ioctl'),
+    (55, 3, 'uadmin'),
+    (56, 0, 'nosys'),
+    (57, 3, 'utssys'),
+    (58, 0, 'nosys'),
+    (59, 3, 'exece'),
+    (60, 1, 'umask'),
+    (61, 1, 'chroot'),
+    (62, 3, 'fcntl'),
+    (63, 2, 'ulimit'),
+    (64, 0, 'nosys'),
+    (65, 0, 'nosys'),
+    (66, 0, 'nosys'),
+    (67, 0, 'nosys'),
+    (68, 0, 'nosys'),
+    (69, 0, 'nosys'),
+    (70, 4, 'advfs'),
+    (71, 1, 'unadvfs'),
+    (72, 4, 'rmount'),
+    (73, 1, 'rumount'),
+    (74, 5, 'rfstart'),
+    (75, 0, 'nosys'),
+    (76, 1, 'rdebug'),
+    (77, 0, 'rfstop'),
+    (78, 6, 'rfsys'),
+    (79, 1, 'rmdir'),
+    (80, 2, 'mkdir'),
+    (81, 4, 'getdents'),
+    (82, 0, 'nosys'),
+    (83, 0, 'nosys'),
+    (84, 3, 'sysfs'),
+    (85, 4, 'getmsg'),
+    (86, 4, 'putmsg'),
+    (87, 3, 'poll')
 ]
 
+#
+# A SystemCall represents an individual call
+#
+SystemCall = namedtuple('SystemCall', \
+                        ['line_num', 'addr', 'call_num', \
+                         'call_name', 'argc', 'args'])
 
 
-class SyscallParser:
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
-    def handle_line(self, line):
-        if re.match('.*MOVW\s+&0x4,%r0', line) or \
-           re.match('.*MOVW.*,%r1', line):
-            self.in_syscall = True
+class SyscallFinder:
 
-        m = re.match('.*MOVW\s+&0x(.*),%r1', line)
-        if m:
-            self.syscall_number = int(m.group(1), 16) / 8
+    def __init__(self, infile):
+        self.pattern = re.compile(r'GATE')
+        self.addr_pattern = re.compile(r'^[a-f0-9]{8} ([a-f0-9]{8})\|')
+        self.infile = infile
+        with open(self.infile, 'r') as f:
+            self.lines = f.readlines()
 
-        sys.stdout.write(line)
-            
-        if re.match('.*GATE', line) and self.in_syscall:
-            if self.syscall_number >= 0 and self.syscall_number < len(calls):
-                print('    <Syscall %s>' % calls[self.syscall_number])
-            else:
-                print('    <Syscall %d>' % (self.syscall_number))
-        else:
-            print
+    def get_syscall(self, gate_line):
+        # The line previous to the GATE will contain the
+        # syscall number.
+        addr = re.match(self.addr_pattern, self.lines[gate_line]).group(1)
+        hexnum = re.split(":? +", self.lines[gate_line - 1])[1]
+        call_num = int(int(hexnum, 16) / 8)
+        argc = syscalls[call_num][1]
+        call_name = syscalls[call_num][2]
 
-        if not re.match('.*MOVW.*&0x4,%r0', line) and \
-           not re.match('.*MOVW.*,%r1', line):
-            self.in_syscall = False
+        return SystemCall(gate_line, addr, call_num, call_name, argc, [])
 
 
-    def main(self, fname):
-        self.in_syscall = False
+    def handle_syscall(self, gate_line):
+        system_call = self.get_syscall(gate_line)
 
-        with open(fname, 'rb') as logfile:
-            for l in logfile:
-                l = l.strip()
-                self.handle_line(l)
+        print(system_call)
+        # for j in range(30, 3, -1):
+        #     sys.stdout.write(self.lines[gate_line - j])
 
+    def find_syscalls(self):
+        for i in range(len(self.lines)):
+            if self.pattern.search(self.lines[i]):
+                self.handle_syscall(i)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: syscalls <logfile>")
+        eprint('usage: syscalls <log_file>');
         exit(1)
 
-    SyscallParser().main(sys.argv[1])
+    infile = sys.argv[1]
+    parser = SyscallFinder(infile)
+    parser.find_syscalls()
